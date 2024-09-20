7be99bd0768bd7da793c67ccc816471c7a3be678
S-J-K-F <Skey@ftstudent.org>
2024-09-20 12:34:48 -0400

    09-20 up 2

--- Scenes/red_bullet_tower.gd
+++ Scenes/red_bullet_tower.gd
@@ -28,15 +28,14 @@ func _on_tower_body_entered(body):
 				if i.get_parent().get_progress() > currTarget.get_progress():
 					currTarget = i.get_node("../")
 
-					
+		curr = currTarget
+		pathName = currTarget.get_parent().name
 
-		#pathName = currTarget.get_parent().name
-
-		#var tempBullet = Bullet.instantiate()
-		#tempBullet.pathName = pathName
-		#tempBullet.bulletDamage = bulletDamage
-		#get_node("BulletContainer").add_child(tempBullet)
-		#tempBullet.global_position = $Aim.global_position
+		var tempBullet = Bullet.instantiate()
+		tempBullet.pathName = pathName
+		tempBullet.bulletDamage = bulletDamage
+		get_node("BulletContainer").add_child(tempBullet)
+		tempBullet.global_position = $Aim.global_position
 
 
 func _on_tower_body_exited(body):
