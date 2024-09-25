39637ca0808a048688149925bcd0dfccc1b760e4
S-J-K-F <Skey@ftstudent.org>
2024-09-25 13:14:27 -0400

    09-25 up 1

---
+++ Scenes/RedTowerPanel.gd
@@ -0,0 +1,6 @@
+extends Panel
+
+
+
+func _on_gui_input(event:InputEvent):
+	pass # Replace with function body.
--- Scenes/red_bullet_tower.gd
+++ Scenes/red_bullet_tower.gd
@@ -10,7 +10,10 @@ var curr
 func _process(delta):
 	if is_instance_valid(curr):
 		self.look_at(curr.global_position)
-
+	else:
+		for i in get_node("BulletContainer").get_child_count():
+			get_node("BulletContainer").get_child(i).queue_free()
+			
 
 func _on_tower_body_entered(body):
 	if "Soldier A" in body.name:
