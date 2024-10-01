929c2e993e17f6312a77d68abe807f7fe2fb150b
S-J-K-F <Skey@ftstudent.org>
2024-10-01 13:16:36 -0400

    09-30 up 2

--- Scenes/RedTowerPanel.gd
+++ Scenes/RedTowerPanel.gd
@@ -18,15 +18,15 @@ func _on_gui_input(event:InputEvent):
 	elif event is InputEventMouseMotion and event.button_mask == 1:
 		#Left click down drag
 		if get_child_count() > 1:
-			get_child(1).global_position = event.global_position
+			get_child(1).global_position = get_global_mouse_position()
 
 			var mapPath = get_tree().get_root().get_node("Main/TileMap")
 			var tile = mapPath.local_to_map(get_global_mouse_position())
 			currTile = mapPath.get_cell_atlas_coords(0, tile, false)
 			if (currTile == Vector2i(4,5)):
-				get_child(1).get_node("Area").modulate = Color(0,255,0)
+				get_child(1).get_node("Area").modulate = Color.BLUE
 			else:
-				get_child(1).get_node("Area").modulate = Color(255,255,255)
+				get_child(1).get_node("Area").modulate = Color.RED
 	elif event is InputEventMouseButton and event.button_mask == 0:
 		#left click up
 		if event.global_position.x >= 1545:
@@ -39,7 +39,7 @@ func _on_gui_input(event:InputEvent):
 				var path = get_tree().get_root().get_node("Main/Towers")
 
 				path.add_child(tempTower)
-				tempTower.global_position = event.global_position
+				tempTower.global_position = get_global_mouse_position()
 				tempTower.get_node("Area").hide()
 	else:
 		if get_child_count() > 1:
